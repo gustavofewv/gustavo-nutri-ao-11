1d9134bc1fa80b2bc4fb675c12d2a21535ce1369
gustavofewv <gustavo.oliveira.alcantara@escola.pr.gov.br>
2025-11-05 09:49:39 -0300

    Add files via upload

--- Tabela-jp-main-real-main/Tabela-jp-main-real-main/Tabela-Nutri-o-main/js/remove-paciente.js
+++ Tabela-jp-main-real-main/Tabela-jp-main-real-main/Tabela-Nutri-o-main/js/remove-paciente.js
@@ -1,7 +1,14 @@
-paciente.forEach(Function(paciente){
-    paciente.addEeventlistener('dbclick',function(){
-        this
-    }
+const tabela = document.querySelector("table");
 
-    )
-}
+tabela.addEventListener('dblclick', function(event){
+   let alvoEvento = event.target;
+   let paiAlvo = alvoEvento.parentNode.remove();
+
+   event.target.parentNode.classlist.add("fadeOut");
+
+   setTimeout (function(){
+    event.target.parentNode.remove();
+   }, 500);
+
+   paiAlvo.remove();
+});
